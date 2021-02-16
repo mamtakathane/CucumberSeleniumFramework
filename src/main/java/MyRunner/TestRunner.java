59b410c3f1d074d8cb3193373f7e87e6002102f9
mamtakathane <46951627+mamtakathane@users.noreply.github.com>
2021-02-16 12:19:26 +0530

    update

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 			features = "/Users/NaveenKhunteta/Documents/workspace/FreeCrmBDDFramework/src/main/java/Features/taggedhooks.feature", //the path of the feature files
 			glue={"stepDefinitions"}, //the path of the step definition files
 			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
-			monochrome = false, //display the console output in a proper readable format
+			monochrome = true, //display the console output in a proper readable format
 			strict = true, //it will check if any step is not defined in step definition file
 			dryRun = false //to check the mapping is proper between feature file and step def file
 			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
