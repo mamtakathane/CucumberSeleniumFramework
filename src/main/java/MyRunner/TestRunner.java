a5affb9f1df55e0c3a0ed3cb84739f1e3a28bf8b
mamtakathane <mamta.kathane@gmail.com>
2021-02-16 14:34:41 +0530

    added new code

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -13,12 +13,13 @@ import cucumber.api.junit.Cucumber;
 			monochrome = true, //display the console output in a proper readable format
 			strict = true, //it will check if any step is not defined in step definition file
 			dryRun = false //to check the mapping is proper between feature file and step def file
-			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
+			,tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
 
 			)
 	 
 	public class TestRunner {
 	 
+		int i =0;
 	}
 	
 	//ORed : tags = {"@SmokeTest , @RegressionTest"} -- execute all tests tagged as @SmokeTest OR @RegressionTest
